e27966416de5661451673ce2898c3f353dd90c43
Maximilian Riley <maximilian@Maximilians-MacBook-Pro.local>
2019-10-17 17:44:01 +0100

    updated form, using conditionals to prompt the user

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,21 +1,31 @@
 <template>
-    <div>
+    <div id="container">
         <form action="http://localhost:3005/users" method="post">
-            <input type="text" name="name" v-model="name" placeholder="first name...">
-            <input type="text" name="email" v-model="email" placeholder="email...">
-            <input type="text" name="password" v-model="password" placeholder="password...">
-            <input type="submit" value="send!">
+            <div id="inputs">
+                <input id="name" class="inputs" type="text" name="name" @change="this.userInputs" v-model="name" placeholder="first name...">
+                <input id="email" class="inputs" type="text" name="email" @change="this.emailIsValid" v-model="email" placeholder="email...">
+                <input id="password" class="inputs" type="text" name="password" @change="this.userInputs" v-model="password" placeholder="password...">
+                <button id="button" class="inputs" type="submit" value="send!">
+                    <font-awesome-icon v-if="this.show === true && this.validity === true" id="check" icon="check-circle"></font-awesome-icon>
+                    <font-awesome-icon v-else-if="this.show === true && this.validity === false" id="cross" icon="times-circle"></font-awesome-icon>
+                    <font-awesome-icon v-else id="userEdit" icon="user-edit"></font-awesome-icon>
+                    
+                        send!
+                    <font-awesome-icon id="arrow" icon="angle-double-right"></font-awesome-icon>
+                </button>
+            </div>
         </form>
         <ul>
-            <li>{{name}}</li>
+            <!-- <li>{{name}}</li>
             <li>{{email}}</li>
             <li>{{password}}</li>
-            <li>{{this.password}}</li>
+            <li>{{this.password}}</li> -->
         </ul>
     </div>
 </template>
 
 <script>
+
     export default {
         name: 'Form',
         data() {
@@ -23,43 +33,84 @@
                 name: '',
                 email: '',
                 password: '',
-                users: {}
+                users: {},
+                show: false,
+                validity: false,
+                inputsEdited: false
             }
         },
         methods: {
-            allUsers: async function() {
-                // let obj = {
-                //     name: this.name,
-                //     email: this.email,
-                //     password: this.password
-                // }
-                // console.log(obj);
-                // try {
-                //     await fetch('http://localhost:3005/users', {
-                //         method: 'post',
-                //         body: JSON.stringify(obj)
-                //     })
-                //     .then((res) => res.json())                    
-                //     .then(function(data) {
-                //         console.log(data);
-                //     });
-                //     // const users = await res.json();
-                //     // this.users = users;
-                // } catch (e) {
-                //     console.log(e)
-                // };
+            emailIsValid: function (email) {
+                email = this.email
+                
+               this.validity = /\S+@\S+\.\S+/.test(email)
+               console.log(`emailIsValid:${this.validity}`)
             },
-            // sendData: async function() {
-            //     try {
-            //         const send = await fetch(`https://localhost:3005/`);
-            //     } catch (e) {
-            //         console.log(e)
-            //     }
-            // }
+            userInputs: function() {
+                if (this.name !== '' && this.email !== '' && this.password !== '') {
+                    this.inputsEdited = true
+                    this.show = true
+                }
+                console.log(`userInputs:${this.inputsEdited}`)
+            },
+
+
         }
     }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+#container {
+    width: 30vw;
+    height: 40vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /* background-color:#ced6e0;
+    padding: 10px;
+    border-radius: 7%; */
+}
+form {
+    height: 75%;
+    width: 75%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color:#5352ed;
+    border-radius: 7%;
+}
+#inputs {
+    display: flex;
+    flex-direction: column;
+    width: 75%;
+    height: 75%;
+    justify-content: space-between
+}
+.inputs {
+    height: 10%;
+    border: 0;
+    padding: 5px;
+    border-radius: 20px;
+    font-size: 2vh;
+    padding-left: 1vw;
+}
 
+#button {
+    display: flex;
+    justify-content: space-between;
+    padding: 0 1vw 0 1vw;
+    align-items: center;
+    color: #747d8c;
+}
+
+#check {
+    color: #2ed573;
+}
+#cross {
+    color: #ff4757;
+}
+
+#arrow {
+    color: #70a1ff;
+}
 </style>
