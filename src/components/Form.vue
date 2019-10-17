272b863b690a6bb5f81274b338a16a765d551984
Maximilian Riley <maximilian@Maximilians-MacBook-Pro.local>
2019-10-17 18:45:13 +0100

    more conditional rendering

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -5,14 +5,14 @@
                 <input id="name" class="inputs" type="text" name="name" @change="this.userInputs" v-model="name" placeholder="first name...">
                 <input id="email" class="inputs" type="text" name="email" @change="this.emailIsValid" v-model="email" placeholder="email...">
                 <input id="password" class="inputs" type="text" name="password" @change="this.userInputs" v-model="password" placeholder="password...">
-                <button id="button" class="inputs" type="submit" value="send!">
+                <button id="button" class="inputs" type="submit" value="send!" ref="button">
                     <font-awesome-icon v-if="this.show === true && this.validity === true" id="check" icon="check-circle"></font-awesome-icon>
                     <font-awesome-icon v-else-if="this.show === true && this.validity === false" id="cross" icon="times-circle"></font-awesome-icon>
                     <font-awesome-icon v-else id="userEdit" icon="user-edit"></font-awesome-icon>
-                    
-                        send!
+                        Go!
                     <font-awesome-icon id="arrow" icon="angle-double-right"></font-awesome-icon>
                 </button>
+                <p v-if="this.validity === false && this.inputsEdited === true" id="indicator">Invalid e-mail address</p>
             </div>
         </form>
         <ul>
@@ -36,24 +36,32 @@
                 users: {},
                 show: false,
                 validity: false,
-                inputsEdited: false
+                inputsEdited: false,
             }
         },
         methods: {
             emailIsValid: function (email) {
                 email = this.email
-                
-               this.validity = /\S+@\S+\.\S+/.test(email)
-               console.log(`emailIsValid:${this.validity}`)
+            
+                this.validity = /\S+@\S+\.\S+/.test(email)
+                console.log(`emailIsValid:${this.validity}`)
+                this.send();
             },
             userInputs: function() {
                 if (this.name !== '' && this.email !== '' && this.password !== '') {
                     this.inputsEdited = true
                     this.show = true
                 }
+
                 console.log(`userInputs:${this.inputsEdited}`)
             },
-
+            send: function() {
+                if (this.validity === true && this.inputsEdited === true) {
+                    let send = this.$refs.button;
+                    send.style.color = '#2ed573';
+                }
+            },
+            
 
         }
     }
@@ -78,6 +86,10 @@ form {
     align-items: center;
     background-color:#5352ed;
     border-radius: 7%;
+}
+input:focus,
+button:focus {
+    outline:none;
 }
 #inputs {
     display: flex;
@@ -112,5 +124,9 @@ form {
 
 #arrow {
     color: #70a1ff;
+}
+
+#indicator {
+    color: #ff4757;
 }
 </style>
